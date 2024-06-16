3e1abec2a7e959478d677ee45ef2d5278997e432
MV9643 <156877871+MV9643@users.noreply.github.com>
2024-06-16 18:29:40 +0530

    saved

---
+++ js/custom.js
@@ -0,0 +1,25 @@
+//  $(document).ready(function(){
+// $('#room-carousel .owl-carousel').owlCarousel({
+// loop: true,
+// margin: 10,
+// nav: true,
+// dots: false,
+// navText: ["<span class='carousel-nav-icon'><i class='bi bi-arrow-left'></i></span>","<span class='carousel-nav-icon'><i class='bi bi-arrow-right'></i></span>"],
+// responsive: {
+//     0: {
+//         items: 1
+//     },
+//     600: {
+//         items: 2
+//     },
+//     1000: {
+//         items: 3
+//     }
+// }
+// });
+// }); 
+
+
+
+
+
